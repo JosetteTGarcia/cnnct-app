f7c54d6aa7ed88e99bf9fb65a93ad7a4fbdabc68
Josette <josettetgarcia@gmail.com>
2023-01-31 20:47:46 -0600

    added associations, no tests yet

--- app/controllers/gatherers_controller.rb
+++ app/controllers/gatherers_controller.rb
@@ -1,2 +1,4 @@
 class GatherersController < ApplicationController
+    belongs_to :gather_attendee, class_name: "User" 
+    belongs_to :attended_gather, class_name: "Gather"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :gatherers, foreign_key: gather_attendee_id
+    has_many :attended_gathers, through: :gatherers
+    has_many :created_gathers, foreign_key: :creator_id, class_name: "Gather"
 end
