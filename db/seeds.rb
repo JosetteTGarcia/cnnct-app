827004ac02eef148b91a6daad9b4ed89823836d1
Josette <josettetgarcia@gmail.com>
2023-02-08 20:59:11 -0600

    added faker gem and added three User accounts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test_users = User.create([
+    { 
+        username: 'firstTestAccount',
+        img: 'https://i.imgur.com/ypKbcbG.jpg',
+        first_name: 'Oliver',
+        last_name: 'Jones',
+        state: 'Texas',
+        city: 'Austin',
+        bio: 'craft beers and fetch',
+    }, 
+    { 
+        username: 'secondTestAccount',
+        img: 'https://i.imgur.com/rFSYtVy.jpg',
+        first_name: 'Frankenstein',
+        last_name: 'Hilton',
+        state: 'Texas',
+        city: 'Corpus Christi',
+        bio: 'Not a monster, just wanna bite',
+    }, 
+    { 
+        username: 'thirdTestAccount',
+        img: 'https://i.imgur.com/jSQpKbM.jpg',
+        first_name: 'Samson',
+        last_name: 'Jones',
+        state: 'Texas',
+        city: 'Lewisville',
+        bio: 'Black coffee, craft beers, and jazz.',
+    }
+])
