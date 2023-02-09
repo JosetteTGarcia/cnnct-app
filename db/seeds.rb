39c60924471cfd8f1d397c4428e184942d8db24d
Josette <josettetgarcia@gmail.com>
2023-02-08 21:08:07 -0600

    seed data for activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-test_users = User.create([
-    { 
-        username: 'firstTestAccount',
-        img: 'https://i.imgur.com/ypKbcbG.jpg',
-        first_name: 'Oliver',
-        last_name: 'Jones',
-        state: 'Texas',
-        city: 'Austin',
-        bio: 'craft beers and fetch',
-    }, 
-    { 
-        username: 'secondTestAccount',
-        img: 'https://i.imgur.com/rFSYtVy.jpg',
-        first_name: 'Frankenstein',
-        last_name: 'Hilton',
-        state: 'Texas',
-        city: 'Corpus Christi',
-        bio: 'Not a monster, just wanna bite',
-    }, 
-    { 
-        username: 'thirdTestAccount',
-        img: 'https://i.imgur.com/jSQpKbM.jpg',
-        first_name: 'Samson',
-        last_name: 'Jones',
-        state: 'Texas',
-        city: 'Lewisville',
-        bio: 'Black coffee, craft beers, and jazz.',
-    }
+# test_users = User.create([
+#     { 
+#         username: 'firstTestAccount',
+#         img: 'https://i.imgur.com/ypKbcbG.jpg',
+#         first_name: 'Oliver',
+#         last_name: 'Jones',
+#         state: 'Texas',
+#         city: 'Austin',
+#         bio: 'craft beers and fetch',
+#     }, 
+#     { 
+#         username: 'secondTestAccount',
+#         img: 'https://i.imgur.com/rFSYtVy.jpg',
+#         first_name: 'Frankenstein',
+#         last_name: 'Hilton',
+#         state: 'Texas',
+#         city: 'Corpus Christi',
+#         bio: 'Not a monster, just wanna bite',
+#     }, 
+#     { 
+#         username: 'thirdTestAccount',
+#         img: 'https://i.imgur.com/jSQpKbM.jpg',
+#         first_name: 'Samson',
+#         last_name: 'Jones',
+#         state: 'Texas',
+#         city: 'Lewisville',
+#         bio: 'Black coffee, craft beers, and jazz.',
+#     }
+# ])
+
+activities = Activity.create([
+    { a_type: 'Coffee' }, 
+    { a_type: 'Talking' },
+    { a_type: 'Walking' },
+    { a_type: 'Yoga' },
+    { a_type: 'Concert'},
+    { a_type: 'Drinks' },
+    { a_type: 'Dancing' },
+    { a_type: 'Art Show' },
+    { a_type: 'Dinner' },
 ])
+
+
+# movies = Movie.create([
+#     { name: 'Star Wars' }, 
+#     { name: 'Lord of the Rings' }
+
+# ])
