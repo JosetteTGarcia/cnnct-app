e638dc868b8bf64ef5478fa7afd67ac49fd8244c
Josette <josettetgarcia@gmail.com>
2023-02-11 15:34:08 -0600

    added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   
+  namespace :api do
+    resources :gathers
+    resources :users
+    resources :activities, only: [:index, :show, :create, :update]
+    resources :interests, only: [:index, :show, :create, :update]
+  
+
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+  end
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
